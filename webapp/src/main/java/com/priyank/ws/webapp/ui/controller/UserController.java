a1622bfc3cb63f08b6d37e869a23d91fc7521ea5
Priyank Gahtori <priyankgahtori@Priyanks-MacBook-Air.local>
2021-01-01 18:58:37 +0530

    reading request parameters

--- webapp/src/main/java/com/priyank/ws/webapp/ui/controller/UserController.java
+++ webapp/src/main/java/com/priyank/ws/webapp/ui/controller/UserController.java
@@ -2,17 +2,27 @@ package com.priyank.ws.webapp.ui.controller;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
+
+    @GetMapping(path = "/{userId}")
+    public String getUser(@PathVariable String userId) {
+        return "GET user called with userId = " + userId;
+    }
+
     @GetMapping
-    public String getUser() {
-        return "GET user called";
+    public String getUser(@RequestParam(value = "page", defaultValue = "1") int page,
+            @RequestParam(value = "limit", defaultValue = "30", required = true) int limit,
+            @RequestParam(value = "sort", required = false, defaultValue = "desc") String sort) {
+        return "GET user called with page = " + page + " limt = " + limit + " Sorting " + sort;
     }
 
     @PostMapping
