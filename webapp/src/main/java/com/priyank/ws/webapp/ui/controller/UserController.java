6813cd5bd109689edf1004e8414d8f7392519d24
Priyank Gahtori <priyankgahtori@Priyanks-MacBook-Air.local>
2021-01-01 18:30:38 +0530

    Basic HTTP methods executed

---
+++ webapp/src/main/java/com/priyank/ws/webapp/ui/controller/UserController.java
@@ -0,0 +1,32 @@
+package com.priyank.ws.webapp.ui.controller;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    @GetMapping
+    public String getUser() {
+        return "GET user called";
+    }
+
+    @PostMapping
+    public String createUser() {
+        return "Create USer was called";
+    }
+
+    @PutMapping
+    public String updateUser() {
+        return "Update User Called";
+    }
+
+    @DeleteMapping
+    public String deleteUser() {
+        return "Delete User Called";
+    }
+}
