3903f65d91f14f17b2d2f647af1fb7534747ee5c
priyank Gahtori <priyank.gahtori@gmail.com>
2021-01-08 18:04:58 +0530

    added account management microservice

---
+++ photoapp-api-account-management/src/test/java/com/priyank/photoapp/api/account/photoappapiaccountmanagement/PhotoappApiAccountManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.priyank.photoapp.api.account.photoappapiaccountmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoappApiAccountManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
